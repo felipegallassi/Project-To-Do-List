1f199f92eebc82e19cd107b5f66c77ff64f31ad7
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-08 13:35:17 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let textoDaTarefa = document.getElementById('texto-tarefa');
 let botãoDaTarefa = document.getElementById('criar-tarefa');
 let listaDeTarefas = document.getElementById('lista-tarefas');
+let ElementoDaLista = document.getElementsByClassName('ElementoDaLista');
 
 function addTarefa () {
     let criarElemento = document.createElement('li');
     criarElemento.innerText = textoDaTarefa.value;
+    criarElemento.className = 'ElementoDaLista';
     listaDeTarefas.appendChild(criarElemento);
     textoDaTarefa.value = '';
 }
 botãoDaTarefa.addEventListener('click', addTarefa);
+
+function AddCinza (event) {
+    for(index = 0; index < ElementoDaLista.length; index += 1){
+        ElementoDaLista[index].style.backgroundColor = 'white';
+    }
+
+
+    for(index = 0; index < ElementoDaLista.length; index += 1){
+       if(event.target === ElementoDaLista[index]){
+          event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+       }
+    }
+}
+listaDeTarefas.addEventListener('click', AddCinza);
+console.log(ElementoDaLista); 
